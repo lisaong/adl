f2c6f0fb7fa78d8a924308b65ec88881375898a9
Lisa <lisaong@nus.edu.sg>
2020-09-16 16:28:57 +0800

    load model

--- HelloWorld/hello_tensorflow_app/app.js
+++ HelloWorld/hello_tensorflow_app/app.js
@@ -3,8 +3,22 @@ const app = express();
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 
+async function loadModel(url) {
+  return await tf.loadLayersModel(url);
+}
+
+// load model from relative path
+var model;
+loadModel('file://model/model.json').then((result) => {
+    model = result;
+});
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  if (model != null) {
+    res.send('<div>' + JSON.stringify(model) + '</div>');
+  } else {
+    res.send('Hello world');
+  }
 });
 
 const port = process.env.PORT || 3000;
