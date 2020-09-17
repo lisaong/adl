fb8cc2b8a8b0244256696c4162b2bfa1af932f7b
Lisa <lisaong@nus.edu.sg>
2020-09-17 17:06:29 +0800

    form post

--- HelloWorld/hello_tensorflow_app/app.js
+++ HelloWorld/hello_tensorflow_app/app.js
@@ -4,6 +4,11 @@ const fs = require('fs')
 const tf = require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 
+// to parse POST requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true})); // parse form data
+app.use(bodyParser.json()) // parse json data
+
 // functions
 async function loadModel(url) {
   return await tf.loadLayersModel(url);
@@ -13,33 +18,40 @@ function loadModelArtifacts(path) {
   return JSON.parse(fs.readFileSync(path, 'utf-8'));
 }
 
-async function predict(model, data) {
+function predict(model, data) {
   let input = tf.tensor(data);
-  return await model.predict(input).array();
+  return model.predict(input).arraySync();
 }
 
 // load model and artifacts from relative path
-let predictions = null;
-let data = null;
-loadModel('file://model/model.json').then(model => {
-    let artifacts = loadModelArtifacts('model_artifacts.json');
-    data = artifacts['X_test'].slice(0, 5);
-    predict(model, data).then(result => {
-      predictions = result;
-    })
+let model = null;
+let artifacts = null;
+loadModel('file://model/model.json').then(m => {
+    artifacts = loadModelArtifacts('model_artifacts.json');
+    model = m;
 });
 
-app.get('/', function (req, res) {
-  if (predictions != null) {
-    var response = '';
-    for (var i=0; i<predictions.length && i < data.length; i++) {
-      response += '<div>' + JSON.stringify(data[i]) + ': '
-               + JSON.stringify(predictions[i]) + '</div>';
-    }
-    res.send(response);
-  } else {
-    res.send('Hello world');
+
+app.get('/', (req, res) => {
+  // form to get predictions
+  res.send('<form action="/" method="post">' +
+   '<div>5-day stock quotes for TSLA (comma-separated):<div/>' +
+   '<input type="text" size=50 name="data" value="70.12,71.114,71.136,70.92,71.118" />' +
+   '<input type="submit" value="Predict day 6"/>' +
+   '</form>');
+});
+
+
+app.post('/', (req, res) => {
+  // convert to floats
+  const ar = req.body.data.split(',');
+  let data = Array();
+  for (var i=0; i<ar.length; i++) {
+    data[i] = parseFloat(ar[i]);
   }
+  let predictions = predict(model, [data]);
+  return res.send('TSLA day 6: ' +
+    JSON.stringify(predictions[0]));
 });
 
 const port = process.env.PORT || 3000;
