033e18952f50f7a41ae4202bb7c5f0da3aa61290
Lisa <lisaong@nus.edu.sg>
2020-09-18 15:15:41 +0800

    outline day 1

--- SMDL/Day1/train.py
+++ SMDL/Day1/train.py
@@ -7,6 +7,21 @@ import json
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+
 from tensorflow.keras.callbacks import ModelCheckpoint
+
+# NLP datasets from https://huggingface.co
+from datasets import load_dataset
+
+# this dataset is large, for demonstration purposes,
+# we'll just load the first 10% of train and validation sets
+train_ds, val_ds = load_dataset('empathetic_dialogues', split=['train[:10%]', 'validation[:10%]'])
+
+print(train_ds, val_ds)
+
+# vectorize the text
+sequence_len = 20 # TODO: this can also be computed based on median length
+vectorizer = TextVectorization(max_tokens=20000, output_sequence_length=20)
 
 
