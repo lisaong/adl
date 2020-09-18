770cfa93da77cce4a8d00873a5433cbffaf46b34
Lisa <lisaong@nus.edu.sg>
2020-09-19 07:57:00 +0800

    load dataset from csv

--- SMDL/Day1/train.py
+++ SMDL/Day1/train.py
@@ -8,20 +8,13 @@ from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
-
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-# NLP datasets from https://huggingface.co
-from datasets import load_dataset
-
-# this dataset is large, for demonstration purposes,
-# we'll just load the first 10% of train and validation sets
-train_ds, val_ds = load_dataset('empathetic_dialogues', split=['train[:10%]', 'validation[:10%]'])
-
-print(train_ds, val_ds)
+df_train = pd.read_csv('./empathetic_dialogues_train.csv')
+print(df_train.head())
+df_val = pd.read_csv('./empathetic_dialogues_val.csv')
+print(df_val.head())
 
 # vectorize the text
 sequence_len = 20 # TODO: this can also be computed based on median length
 vectorizer = TextVectorization(max_tokens=20000, output_sequence_length=20)
-
-
