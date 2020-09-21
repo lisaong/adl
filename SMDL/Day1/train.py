7382d273c548f4b773cbf7e8528023c81b83ddc3
Lisa <lisaong@nus.edu.sg>
2020-09-21 13:54:37 +0800

    prepare dataset

--- SMDL/Day1/train.py
+++ SMDL/Day1/train.py
@@ -3,18 +3,62 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
-import json
-from sklearn.model_selection import train_test_split
-from tensorflow.keras.models import Model, load_model
-from tensorflow.keras.layers import Input, Dense
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
+from sklearn.preprocessing import LabelEncoder
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, GRU
 from tensorflow.keras.callbacks import ModelCheckpoint
+from nltk import word_tokenize
+import pickle
 
-df_train = pd.read_csv('./empathetic_dialogues_train.csv')
+# globals
+MODEL_ARTIFACTS = dict()
+
+
+def save_artifacts(key_values: dict, dest='model_artifacts.pkl'):
+    MODEL_ARTIFACTS.update(key_values)
+    pickle.dump(MODEL_ARTIFACTS, open(dest, 'wb'))
+
+
+df_train = pd.read_csv('./empathetic_dialogues_train.csv', index_col=0)
 print(df_train.head())
-df_val = pd.read_csv('./empathetic_dialogues_val.csv')
+df_val = pd.read_csv('./empathetic_dialogues_val.csv', index_col=0)
 print(df_val.head())
 
+# compute median length of the text
+sequence_len = df_train['prompt'].apply(lambda x: len(word_tokenize(x))).median()
+print(f'Sequence length: {sequence_len}')
+
+# visualise the imbalance
+plt.hist(df_train['context'], label='train')
+plt.xticks(rotation=45, ha='right')
+plt.hist(df_val['context'], label='validation')
+plt.xticks(rotation=45, ha='right')
+plt.legend()
+plt.show()
+
+print(df_train['context'].value_counts())
+print(df_val['context'].value_counts())
+
 # vectorize the text
-sequence_len = 20 # TODO: this can also be computed based on median length
-vectorizer = TextVectorization(max_tokens=20000, output_sequence_length=20)
+# the dataset contains repetition of prompts, that's ok as long as the labels are consistent.
+# In NN training, as long as we shuffle the data, we should be fine even if the same data
+# is seen multiple times.
+vectorizer = TextVectorization(max_tokens=20000, output_sequence_length=int(sequence_len))
+vectorizer.adapt(df_train['prompt'].values)
+X_train = vectorizer(df_train['prompt']).numpy()
+X_val = vectorizer(df_val['prompt']).numpy()
+print(vectorizer.get_config()) # configuration
+print(vectorizer.get_vocabulary()[:10]) # first 10 words
+save_artifacts({'vectorizer_vocab': vectorizer.get_vocabulary()})
+save_artifacts({'vectorizer_config': vectorizer.get_config()})
+
+# encode and convert labels to categorical
+le = LabelEncoder()
+y_train = to_categorical(le.fit_transform(df_train['context']))
+y_val = to_categorical(le.transform(df_val['context']))
+print(y_train)
+print(y_val)
+print(le.classes_)
+save_artifacts({'label_encoder': le})
