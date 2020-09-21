1d6a1ad091c0107f2727f916d97c3d982f300916
Lisa <lisaong@nus.edu.sg>
2020-09-21 14:08:50 +0800

    initial model

--- SMDL/Day1/train.py
+++ SMDL/Day1/train.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.layers import Input, Embedding, GRU, Flatten, Dense
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Input, Dense, GRU
 from tensorflow.keras.callbacks import ModelCheckpoint
 from nltk import word_tokenize
 import pickle
@@ -27,7 +27,7 @@ df_val = pd.read_csv('./empathetic_dialogues_val.csv', index_col=0)
 print(df_val.head())
 
 # compute median length of the text
-sequence_len = df_train['prompt'].apply(lambda x: len(word_tokenize(x))).median()
+sequence_len = int(df_train['prompt'].apply(lambda x: len(word_tokenize(x))).median())
 print(f'Sequence length: {sequence_len}')
 
 # visualise the imbalance
@@ -45,7 +45,7 @@ print(df_val['context'].value_counts())
 # the dataset contains repetition of prompts, that's ok as long as the labels are consistent.
 # In NN training, as long as we shuffle the data, we should be fine even if the same data
 # is seen multiple times.
-vectorizer = TextVectorization(max_tokens=20000, output_sequence_length=int(sequence_len))
+vectorizer = TextVectorization(max_tokens=20000, output_sequence_length=sequence_len)
 vectorizer.adapt(df_train['prompt'].values)
 X_train = vectorizer(df_train['prompt']).numpy()
 X_val = vectorizer(df_val['prompt']).numpy()
@@ -62,3 +62,30 @@ print(y_train)
 print(y_val)
 print(le.classes_)
 save_artifacts({'label_encoder': le})
+
+# define our model
+vocab_len = len(vectorizer.get_vocabulary())
+embedding_len = 50
+model_input = Input(shape=(sequence_len,), dtype='int64')
+x = Embedding(vocab_len, embedding_len, input_length=sequence_len)(model_input)
+x = GRU(16, activation='tanh', return_sequences=True)(x)
+x = GRU(16, activation='tanh')(x)
+x = Flatten()(x)
+x = Dense(16, activation='relu')(x)
+x = Dense(len(le.classes_), activation='softmax')(x)
+model = Model(model_input, x)
+model.summary()
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+mc = ModelCheckpoint('gru.h5', monitor='val_loss', save_best_only=True)
+
+history = model.fit(X_train, y_train, epochs=5,
+                    validation_data=(X_val, y_val),
+                    callbacks=[mc])
+
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='validation')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
