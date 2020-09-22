5d162f413b8e79681c54e239d45ece008c666a47
Lisa Ong <lisaong@nus.edu.sg>
2020-09-22 11:46:12 +0800

    add chat interface

--- SMDL/Day1/app/demo/views.py
+++ SMDL/Day1/app/demo/views.py
@@ -6,13 +6,13 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
-class MyForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+class ChatInputForm(FlaskForm):
+    name = StringField('Reply', validators=[DataRequired()])
 
 
 @app.route('/', methods=('GET', 'POST'))
 def default():
-    form = MyForm()
+    form = ChatInputForm()
     if form.validate_on_submit():
         return redirect('/')
     return render_template("default.html", form=form)
