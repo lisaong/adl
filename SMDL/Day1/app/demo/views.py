e8833ba41e2cd860c7d30d8eb5405bff1c937cf3
Lisa Ong <lisaong@nus.edu.sg>
2020-09-22 13:09:30 +0800

    load model, get predictions

--- SMDL/Day1/app/demo/views.py
+++ SMDL/Day1/app/demo/views.py
@@ -1,5 +1,6 @@
 from flask import redirect, render_template
 from demo import app
+from demo import ml
 
 from flask_wtf import FlaskForm
 from wtforms import StringField
@@ -7,12 +8,18 @@ from wtforms.validators import DataRequired
 
 
 class ChatInputForm(FlaskForm):
-    name = StringField('Reply', validators=[DataRequired()])
+    reply = StringField('Reply', validators=[DataRequired()])
+
+
+model = ml.TFModel()
 
 
 @app.route('/', methods=('GET', 'POST'))
 def default():
     form = ChatInputForm()
     if form.validate_on_submit():
+        print(form.reply.data)
+        result = model.predict(form.reply.data)
+        print(result)
         return redirect('/')
     return render_template("default.html", form=form)
