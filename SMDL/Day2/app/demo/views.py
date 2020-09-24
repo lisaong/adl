cc61a92d98bf530beae48c748df323762e9caa1d
Lisa Ong <lisaong@nus.edu.sg>
2020-09-24 17:38:53 +0800

    random reply generator

---
+++ SMDL/Day2/app/demo/views.py
@@ -0,0 +1,27 @@
+from flask import redirect, render_template, request, json
+from demo import app
+from demo import ml
+import os
+import random
+
+
+model_dir = os.path.join(os.getcwd(), 'demo', 'model')
+model = ml.TFModel(model_dir=model_dir)
+
+
+@app.route('/', methods=['GET'])
+def default():
+    return render_template("default.html")
+
+
+@app.route('/reply', methods=['POST'])
+def reply_chat():
+    text = request.form['reply']
+
+    # use a random word from the reply text
+    tokens = text.split()
+    seed_word = random.sample(tokens, k=1)
+
+    predictions = model.predict(seed_word)
+    print(predictions)
+    return json.dumps({'predictions': predictions})
