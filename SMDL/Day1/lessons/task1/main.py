d39f54a0dea748a888fe9dc4efb5cf0b85cac3be
Lisa <lisaong@nus.edu.sg>
2020-09-26 08:01:43 +0800

    show normalization of text

--- SMDL/Day1/lessons/task1/main.py
+++ SMDL/Day1/lessons/task1/main.py
@@ -3,7 +3,7 @@
 from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 import numpy as np
 
-my_text = np.array(["hello this is a demonstration of text vectorization"])
+my_text = np.array(["Hello! this is a demonstration of Text Vectorization"])
 
 vectorizer = TextVectorization(max_tokens=100, output_sequence_length=10)
 vectorizer.adapt(my_text)
