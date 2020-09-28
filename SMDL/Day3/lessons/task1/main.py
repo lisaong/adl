61986bba7e6a37f1cad5503837cebe3ecb90e577
Lisa <lisaong@nus.edu.sg>
2020-09-28 23:52:18 +0800

    moved vectorization into Encoder

--- SMDL/Day3/lessons/task1/main.py
+++ SMDL/Day3/lessons/task1/main.py
@@ -6,11 +6,6 @@ from tensorflow.keras.layers.experimental.preprocessing import TextVectorization
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Embedding, GRU
 
-# source text that we want to encode
-# see next task for the target text
-english_text = ['Ask, and it will be given to you',
-                'seek, and you will find',
-                'knock, and it will be opened to you.']
 
 BATCH_SIZE = 3
 EMBEDDING_SIZE = 4
@@ -19,28 +14,22 @@ BOTTLENECK_UNITS = 2
 START_TOKEN = 'aaaaa'
 END_TOKEN = 'zzzzz'
 
-# append start and end tokens, this will indicate when translation should start & stop
-src_text = [f'{START_TOKEN} {t} {END_TOKEN}' for t in english_text]
-
-src_vectorizer = TextVectorization()
-src_vectorizer.adapt(src_text)
-src_sequences = src_vectorizer(src_text)
-src_vocab_size = len(src_vectorizer.get_vocabulary())
-
 
 # Encoder
 # In the final task we'll look at adding Attention layers. To enable an easier comparison
 # we'll wrap the Encoder models into classes
 
 class MyEncoder(Model):
-    def __init__(self, vocab_size, embedding_dim, enc_units, batch_size):
+    def __init__(self, vectorizer, embedding_dim, enc_units, batch_size):
         super(MyEncoder, self).__init__()
+        self.vectorizer = vectorizer
         self.batch_size = batch_size
         self.enc_units = enc_units
-        self.embedding = Embedding(vocab_size, embedding_dim)
+        self.embedding = Embedding(len(vectorizer.get_vocabulary()), embedding_dim)
         self.gru = GRU(self.enc_units, return_state=True)
 
     def call(self, x, hidden):
+        x = self.vectorizer(x)
         x = self.embedding(x)
         output, state = self.gru(x, initial_state=hidden)
         return output, state
@@ -55,11 +44,22 @@ class MyEncoder(Model):
 
 # test
 if __name__ == '__main__':  # so that we can import this file without running the test code
-    encoder = MyEncoder(src_vocab_size, embedding_dim=EMBEDDING_SIZE,
+
+    # source text that we want to encode
+    # see next task for the target text
+    english_text = ['Ask, and it will be given to you',
+                    'seek, and you will find',
+                    'knock, and it will be opened to you.']
+    texts_delimited = [f'{START_TOKEN} {t} {END_TOKEN}' for t in english_text]
+    vectorizer = TextVectorization()
+    vectorizer.adapt(texts_delimited)
+    print('Vocabulary', vectorizer.get_vocabulary())
+
+    encoder = MyEncoder(vectorizer, embedding_dim=EMBEDDING_SIZE,
                         enc_units=BOTTLENECK_UNITS,
                         batch_size=BATCH_SIZE)
     sample_hidden = encoder.initialize_hidden_state()
-    sample_output, sample_hidden = encoder(src_sequences, sample_hidden)
+    sample_output, sample_hidden = encoder(english_text, sample_hidden)
     print(f'Encoder output shape: (batch size, sequence length, units) {sample_output.shape}')
 
     print('========================')
