b73e0def0886b3da495bea450dc9c0dfb1abd09e
Lisa <lisaong@nus.edu.sg>
2020-09-29 10:09:32 +0800

    try a summarization dataset

--- SMDL/Day3/train/train.py
+++ SMDL/Day3/train/train.py
@@ -1,2 +1,13 @@
-# Day 3 - Text Translation using Encoder/Decoder, with optional Attention
+# Day 3 - Neural Machine Translation (Enron Emails)
+
+# Instructions:
+# 1. Go through the lessons before you start
+# 2. Replace _ANS_ with your answers so that the code will run
+# 3. Submit your completed train.py with learning_curve.png
+
+
+
+
+
+
 
