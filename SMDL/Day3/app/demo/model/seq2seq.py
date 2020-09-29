65f25720a73a2a3b0a3eb961fa14fb9475b37230
Lisa <lisaong@nus.edu.sg>
2020-09-29 13:54:09 +0800

    put model definition in a separate file

---
+++ SMDL/Day3/app/demo/model/seq2seq.py
@@ -0,0 +1,60 @@
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Embedding, GRU, Dense
+
+
+class MyEncoder(Model):
+    def __init__(self, vocab_size, embedding_dim, enc_units, batch_size):
+        super(MyEncoder, self).__init__()
+        self.vocab_size = vocab_size
+        self.batch_size = batch_size
+        self.enc_units = enc_units
+        self.embedding = Embedding(vocab_size, embedding_dim)
+        self.gru = GRU(self.enc_units, return_state=True)
+
+    def call(self, x, hidden):
+        x = self.embedding(x)
+        output, state = self.gru(x, initial_state=hidden)
+        return output, state
+
+    def initialize_hidden_state(self):
+        return tf.zeros((self.batch_size, self.enc_units))
+
+    def get_config(self):
+        # to enable model saving as HDF5 format
+        return {'batch_size': self.batch_size,
+                'enc_units': self.enc_units}
+
+
+class MyDecoder(Model):
+    def __init__(self, vocab_size, embedding_dim, dec_units, batch_size):
+        super(MyDecoder, self).__init__()
+        self.batch_size = batch_size
+        self.dec_units = dec_units
+        self.embedding = Embedding(vocab_size, embedding_dim)
+        self.gru = GRU(self.dec_units, return_sequences=True, return_state=True)
+        self.fc = Dense(vocab_size)
+
+    def call(self, x, hidden, enc_output):
+        # x shape after passing through embedding == (batch_size, 1, embedding_dim)
+        x = self.embedding(x)
+
+        # enc_output shape == (batch_size, encoding_dim)
+        # tf.expand_dims(enc_output, 1) shape == (batch_size, 1, encoding_dim)
+        # x shape after concatenation == (batch_size, 1, embedding_dim + encoding_dim)
+        x = tf.concat([tf.expand_dims(enc_output, 1), x], axis=-1)
+
+        # passing the concatenated vector to the GRU
+        output, state = self.gru(x)
+
+        # output shape == (batch_size * 1, hidden_size)
+        output = tf.reshape(output, (-1, output.shape[2]))
+
+        # output shape == (batch_size, vocab)
+        x = self.fc(output)
+
+        return x, state
+
+    def get_config(self):
+        # to enable model saving as HDF5 format
+        return {'batch_size': self.batch_size,
+                'dec_units': self.dec_units}
