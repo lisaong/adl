6544e74be298e75e873b09aee29b0ba5351e0b5c
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2020-10-02 21:35:29 +0800

    Create project_template_yourname.py

---
+++ SMDL/project/project_template_yourname.py
@@ -0,0 +1,64 @@
+###########################################################
+# SMDL Final Project Template
+###########################################################
+# 1. Please complete all sections
+# 2. Minimum requirements: implement and train a model using Tensorflow,
+#    Example project ideas:
+#    - Text Prediction or Generation
+#    - Time Series Prediction or Generation
+#    - Video Classification
+#    - Neural Machine Translation
+# 3. Bonus task: call your model from a Flask application
+#
+###########################################################
+# Name (matching course registration):
+#
+# Title of Project:
+#
+# Business Problem to be Solved:
+#
+# Dataset Source(s):
+#
+# Model Inputs:
+#
+# Model Targets:
+#
+###########################################################
+# Preprocessing (python code)
+
+
+
+
+
+
+
+
+###########################################################
+# Model Architecture and Training (python code)
+ 
+
+
+
+
+
+
+###########################################################
+# Evaluation Metrics (python code)
+
+
+
+
+
+
+###########################################################
+# Observations (written)
+# 
+#
+#
+#
+###########################################################
+# Further Improvements (written)
+# 
+#
+#
+#
