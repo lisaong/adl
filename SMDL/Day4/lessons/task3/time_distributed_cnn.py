32334f1c8388474115b7a6696cb252474c1b5978
Lisa <lisaong@nus.edu.sg>
2020-10-03 18:09:45 +0800

    task 3 start

--- SMDL/Day4/lessons/task3/time_distributed_cnn.py
+++ SMDL/Day4/lessons/task3/time_distributed_cnn.py
@@ -7,13 +7,13 @@ BATCHES_PER_EPOCH = 5
 
 if __name__ == "__main__":
     # load dataset saved from previous task
-    X = np.load('../task2/X.npy', )
-
+    X = np.load('../task2/X.npy', allow_pickle=True)
+    y = np.load('../task2/y.npy', allow_pickle=True)
 
     # split into train and validation
     X_train, X_val, y_train, y_val = train_test_split(X, y, stratify=y)
 
-    # create training dataset
+    # create batched training dataset
     train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)). \
         batch(BATCH_SIZE).repeat(BATCHES_PER_EPOCH)
 
