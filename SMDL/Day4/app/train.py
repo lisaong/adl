895eaa2e29ae245c85d8770749d75eb6cfaadf49
Lisa <lisaong@nus.edu.sg>
2020-10-03 21:18:59 +0800

    update train.py

--- SMDL/Day4/app/train.py
+++ SMDL/Day4/app/train.py
@@ -5,10 +5,17 @@
 # 2. Search for the TODOs, replace _ANS_ with your answers so that the code will run
 # 3. Submit your completed train.py with learning_curve.png
 
-import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import load_model
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+import pickle
 import os
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
 
 from make_dataset import download_dataset
 from td_cnn_rnn import create_model
@@ -24,92 +31,66 @@ def save_artifacts(key_values: dict, dest='model_artifacts.pkl'):
     pickle.dump(MODEL_ARTIFACTS, open(os.path.join(MODEL_DIR, dest), 'wb'))
 
 
-df_train = pd.read_csv('../data/empathetic_dialogues_train.csv', index_col=0)
-print(df_train.head())
-print(df_train.info())
+if __name__ == "__main__":
+    sequence_len = 20
+    batch_size = 32
+    batches_per_epoch = 10
 
-df_val = pd.read_csv('../data/empathetic_dialogues_val.csv', index_col=0)
-print(df_val.head())
-print(df_val.info())
+    dataset_info = {
+        'Archery': {
+            'filename': 'Archery.zip',
+            'url': 'https://github.com/lisaong/mldds-courseware/raw/master/data/ucf101-5classes/train/Archery.zip'
+        },
+        'Basketball': {
+            'filename': 'Basketball.zip',
+            'url': 'https://github.com/lisaong/mldds-courseware/raw/master/data/ucf101-5classes/train/Basketball.zip'
+        },
+        'CricketBowling': {
+            'filename': 'CricketBowling.zip',
+            'url': 'https://github.com/lisaong/mldds-courseware/raw/master/data/ucf101-5classes/train/CricketBowling.zip'
+        }
+    }
 
-# compute median length of the text
-sequence_len = int(df_train['prompt'].apply(lambda x: len(word_tokenize(x))).median())
-print(f'Sequence length: {sequence_len}')
+    X, y = download_dataset(dataset_info, sequence_len)
 
-# visualise the imbalance
-plt.title('Label Counts')
-plt.hist(df_train['context'], label='train')
-plt.xticks(rotation=45, ha='right')
-plt.hist(df_val['context'], label='validation')
-plt.xticks(rotation=45, ha='right')
-plt.legend()
-plt.show()
+    # label encode targets
+    le = LabelEncoder()
+    y_cat = to_categorical(le.fit_transform(y))
+    num_classes = len(le.classes_)
 
-print(df_train['context'].value_counts())
-print(df_val['context'].value_counts())
+    # split to train and test
+    X_train, X_val, y_train, y_val = train_test_split(X, y_cat, stratify=y)
 
-# vectorize the text
-# the dataset contains repetition of prompts, that's ok as long as the labels are consistent.
-# In NN training, as long as we shuffle the data, we should be fine even if the same data
-# is seen multiple times.
-vectorizer = TextVectorization(max_tokens=20000, output_sequence_length=sequence_len)
-vectorizer.adapt(df_train['prompt'].values)
-X_train = vectorizer(df_train['prompt']).numpy()
-X_val = vectorizer(df_val['prompt']).numpy()
-print(vectorizer.get_config()) # configuration
-print(vectorizer.get_vocabulary()[:10]) # first 10 words
-save_artifacts({'vectorizer_vocab': vectorizer.get_vocabulary()})
-save_artifacts({'vectorizer_config': vectorizer.get_config()})
+    # create batched training dataset
+    train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)). \
+        batch(batch_size).repeat(batches_per_epoch)
 
-# encode and convert labels to categorical
-le = LabelEncoder()
-y_train = to_categorical(le.fit_transform(df_train['context']))
-y_val = to_categorical(le.transform(df_val['context']))
-print(y_train)
-print(y_val)
-print(le.classes_)
-save_artifacts({'label_encoder': le})
+    # create model
+    height, width, channels = X.shape[2], X.shape[3], X.shape[4]
+    model = create_model(height, width, channels, 16, num_classes)
 
-# define our model
-vocab_len = len(vectorizer.get_vocabulary())
-embedding_len = 75
-num_classes = len(le.classes_)
-model_input = Input(shape=(sequence_len,), dtype='int64')
+    # train model
+    model_path = os.path.join(MODEL_DIR, 'td_cnn_rnn.h5')
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+    mc = ModelCheckpoint(model_path, save_best_only=True, monitor='val_loss')
+    history = model.fit(train_ds, epochs=5,
+                        validation_data=(X_val, y_val),
+                        callbacks=[mc])
 
-# TODO: Replace _ANS_ with your answers
-x = Embedding(input_dim=_ANS_, output_dim=_ANS_)(model_input)
-x = LSTM(_ANS_, activation=_ANS_)(x)
-x = Flatten()(x)
-x = Dense(_ANS_, activation='relu')(x)
-x = Dropout(.5)(x)
-x = Dense(_ANS_, activation='softmax')(x)
+    plt.plot(history.history['loss'], label='train')
+    plt.plot(history.history['val_loss'], label='validation')
+    plt.title('Learning Curve')
+    plt.xlabel('epochs')
+    plt.ylabel('loss')
+    plt.legend()
+    plt.savefig('learning_curve.png')
+    plt.show()
 
-model = Model(model_input, x)
-model.summary()
+    # save artifacts
+    save_artifacts({'label_encoder': le,
+                    'sequence_len': sequence_len})
 
-model.compile(optimizer='adam', loss='categorical_crossentropy',
-              metrics=['acc'])
-
-# periodically saves the best model every epoch
-mc = ModelCheckpoint(os.path.join(MODEL_DIR, 'lstm.h5'),
-                     monitor='val_acc', save_best_only=True)
-
-history = model.fit(X_train, y_train, epochs=20, batch_size=32, shuffle=True,
-                    validation_data=(X_val, y_val),
-                    callbacks=[mc])
-
-plt.plot(history.history['acc'], label='train')
-plt.plot(history.history['val_acc'], label='validation')
-plt.title('Learning Curve')
-plt.xlabel('epochs')
-plt.ylabel('accuracy')
-plt.legend()
-plt.savefig('learning_curve.png')
-plt.show()
-
-# get metrics
-best_model = load_model(os.path.join(MODEL_DIR, 'lstm.h5'))
-y_pred_train = best_model.predict(X_train)
-y_pred_val = best_model.predict(X_val)
-print(classification_report(y_train.argmax(axis=1), y_pred_train.argmax(axis=1)))
-print(classification_report(y_val.argmax(axis=1), y_pred_val.argmax(axis=1)))
+    # metrics
+    best_model = load_model(model_path)
+    y_pred = best_model.predict(X_val)
+    print(classification_report(y_val.argmax(axis=1), y_pred.argmax(axis=1)))
