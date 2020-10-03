e7039bcd0d631f5d7455653a5ce4498e1f5647a1
Lisa <lisaong@nus.edu.sg>
2020-10-03 23:53:07 +0800

    stream video!

--- SMDL/Day4/app/demo/views.py
+++ SMDL/Day4/app/demo/views.py
@@ -1,7 +1,8 @@
-from flask import render_template, request, url_for
+from flask import render_template, request, Response, session
 from demo import app
 from demo import ml
 import os
+import cv2
 
 # https://flask.palletsprojects.com/en/1.1.x/patterns/fileuploads/
 from werkzeug.utils import secure_filename
@@ -26,10 +27,38 @@ def predict():
     prediction, probability = model.predict(complete_filename)
     print(prediction, probability)
 
-    base_url = url_for('default', _external=True)
-    filename_url = f'{base_url}uploads/{filename}'
-    print(filename_url)
-
+    session['video_path'] = complete_filename
     return render_template('result.html',
-                           prediction=f'{prediction} {probability:.3f}',
-                           url=filename_url)
+                           prediction=f'{prediction} {probability:.3f}')
+
+
+class VideoRenderer(object):
+    def __init__(self, video_path):
+        # capture video
+        self.video = cv2.VideoCapture(video_path)
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        # extract a frame
+        ret, frame = self.video.read()
+
+        # encode raw frame to JPEG and display it
+        ret, jpeg = cv2.imencode('.jpg', frame)
+        return jpeg.tobytes()
+
+
+def gen(camera):
+    while True:
+        # render each frame
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    video_path = session.get('video_path', None)
+    return Response(gen(VideoRenderer(video_path)),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
