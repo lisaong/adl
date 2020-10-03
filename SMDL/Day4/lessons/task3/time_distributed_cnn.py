0309290a87a35eff1b022e756734dc2e1218db5e
Lisa <lisaong@nus.edu.sg>
2020-10-03 18:56:09 +0800

    model training

--- SMDL/Day4/lessons/task3/time_distributed_cnn.py
+++ SMDL/Day4/lessons/task3/time_distributed_cnn.py
@@ -4,6 +4,7 @@ from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Input, GlobalAveragePooling2D, \
     TimeDistributed, GRU, Dense
+from tensorflow.keras.callbacks import ModelCheckpoint
 from sklearn.preprocessing import LabelEncoder
 
 import numpy as np
@@ -11,26 +12,10 @@ import numpy as np
 BATCH_SIZE = 16
 BATCHES_PER_EPOCH = 5
 
-if __name__ == "__main__":
-    # load dataset saved from previous task
-    X = np.load('../task2/X.npy', allow_pickle=True)
-    y = np.load('../task2/y.npy', allow_pickle=True)
 
-    # encode y from labels to binary
-    le = LabelEncoder()
-    y_enc = le.fit_transform(y.ravel())  # convert y from 2D to 1D for encoding
-
-    # split into train and validation
-    X_train, X_val, y_train, y_val = train_test_split(X, y_enc, stratify=y_enc)
-
-    # create batched training dataset
-    train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)). \
-        batch(BATCH_SIZE).repeat(BATCHES_PER_EPOCH)
-
-    height, width, channels = X_train.shape[2], X_train.shape[3], X_train.shape[4]
-
+def create_model(h, w, c):
     # load pre-trained model, set to non-trainable
-    base_model = MobileNetV2(input_shape=(height, width, channels), include_top=False)
+    base_model = MobileNetV2(input_shape=(h, w, c), include_top=False)
     base_model.trainable = False
     x = GlobalAveragePooling2D()(base_model.output)  # compute batch average
     cnn_model = Model(base_model.input, x)
@@ -45,4 +30,33 @@ if __name__ == "__main__":
 
     model = Model(model_input, x)
     model.summary()
+    return model
 
+
+if __name__ == "__main__":
+    # load dataset saved from previous task
+    X = np.load('../task2/X.npy', allow_pickle=True)
+    y = np.load('../task2/y.npy', allow_pickle=True)
+
+    # encode y from labels to binary
+    le = LabelEncoder()
+    y_enc = le.fit_transform(y.ravel())  # convert y from 2D to 1D for encoding
+
+    # split into train and validation
+    X_train, X_val, y_train, y_val = train_test_split(X, y_enc, stratify=y_enc)
+
+    height, width, channels = X_train.shape[2], X_train.shape[3], X_train.shape[4]
+
+    model = create_model(height, width, channels)
+
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+    # create batched training dataset
+    train_ds = tf.data.Dataset.from_tensor_slices((X_train, y_train)). \
+        batch(BATCH_SIZE).repeat(BATCHES_PER_EPOCH)
+
+    mc = ModelCheckpoint('td_cnn_rnn.h5', save_best_only=True, monitor='val_loss')
+
+    history = model.fit(train_ds, epochs=5,
+                        validation_data=(X_val, y_val),
+                        callbacks=[mc])
