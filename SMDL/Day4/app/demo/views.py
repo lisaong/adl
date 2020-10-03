28c8529bc73e8b6c09a12f75399d2bcd5537ebdb
Lisa <lisaong@nus.edu.sg>
2020-10-03 23:58:58 +0800

    stream resized video!

--- SMDL/Day4/app/demo/views.py
+++ SMDL/Day4/app/demo/views.py
@@ -27,11 +27,14 @@ def predict():
     prediction, probability = model.predict(complete_filename)
     print(prediction, probability)
 
+    # save the full video path on the client session to be rendered
+    # by the video_feed route
     session['video_path'] = complete_filename
     return render_template('result.html',
                            prediction=f'{prediction} {probability:.3f}')
 
 
+# https://blog.miguelgrinberg.com/post/video-streaming-with-flask
 class VideoRenderer(object):
     def __init__(self, video_path):
         # capture video
@@ -43,18 +46,26 @@ class VideoRenderer(object):
     def get_frame(self):
         # extract a frame
         ret, frame = self.video.read()
-
-        # encode raw frame to JPEG and display it
-        ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+        if ret:
+            # encode raw frame to JPEG and display it
+            frame = cv2.resize(frame, (160, 160), interpolation=cv2.INTER_LINEAR)
+            ret, jpeg = cv2.imencode('.jpg', frame)
+            return jpeg.tobytes()
+        else:
+            return None
 
 
 def gen(camera):
+    # generator for the video feed
     while True:
         # render each frame
         frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        if frame:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+        else:
+            # no more frames, stop
+            break
 
 
 @app.route('/video_feed')
